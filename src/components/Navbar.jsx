1f958c504abf60d5b7b1ebb33fffc23ad6651e5b
ola-nishant <nishant.4716403221@ipu.ac.in>
2024-07-17 20:47:46 +0530

    added animation on hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,32 @@
 import React from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link } from "@nextui-org/react";
 import MainIcon from "@/components/svg-icons/MainIcon"
+import { motion } from "framer-motion";
+
+const hrVariants = {
+    normal: {
+        width: 0,
+        transition: {
+            type: "spring",
+            stiffness: 100,
+            damping: 20,
+            duration: 0.5
+        }
+    },
+    expanded: {
+        width: "100%",
+        transition: {
+            type: "spring",
+            stiffness: 100,
+            damping: 20,
+            duration: 0.5
+        }
+    }
+};
 
 export default function Nav() {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const [hoveredIndex, setHoveredIndex] = React.useState(null);
 
     const menuItems = [
         "Home",
@@ -12,6 +35,10 @@ export default function Nav() {
         "Selected Projects",
         "Let's Connect",
     ];
+
+    const handleInteraction = (index) => {
+        setHoveredIndex(index);
+    };
 
     return (
         <Navbar isBordered isBlurred={false} onMenuOpenChange={setIsMenuOpen} className='h-[5rem] md:px-[15vw] px-[5vw]'>
@@ -25,7 +52,11 @@ export default function Nav() {
             </NavbarContent>
             <NavbarMenu className="lg:py-[90px] py-[60px]">
                 {menuItems.map((item, index) => (
-                    <NavbarMenuItem key={index}>
+                    <NavbarMenuItem
+                        key={index}
+                        onMouseEnter={() => handleInteraction(index)}
+                        onMouseLeave={() => handleInteraction(null)}
+                    >
                         <Link
                             className="w-full"
                             href="#"
@@ -37,6 +68,12 @@ export default function Nav() {
                                     <h4 className="md:text-[54px] text-[36px] font-semibold text-black tracking-tighter">{item}</h4>
                                 </div>
                                 <hr className="md:my-6 my-2 min-w-full border-b-1" />
+                                <motion.div
+                                    className="border-b-3 border-black relative md:bottom-6 bottom-2"
+                                    initial="normal"
+                                    animate={hoveredIndex === index ? "expanded" : "normal"}
+                                    variants={hrVariants}
+                                />
                             </div>
                         </Link>
                     </NavbarMenuItem>
